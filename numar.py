f761a52e3994db133c9bd4e4687030ff754c0a0b
Stanciu Alexandru <alexandru_stanciu20@yahoo.com>
2024-01-20 19:04:19 +0200

    Adaugat inca un numar random

--- numar.py
+++ numar.py
@@ -5,3 +5,7 @@ numar_random = random.randint(1, 10)
 for i in range(5):
     numar_random = random.randint(1,150)
     print(numar_random)
+
+for i in range(10):
+    numar_randomm = random.randint(1, 250)
+    print(numar_randomm)
